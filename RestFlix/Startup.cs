b235916e48349b8203e5b88c9e2c4e23dab1c73f
Bruno Muller Aguiar <bruno@Brunos-MacBook-Air.local>
2021-03-02 09:47:09 -0300

    Troca versao do .netcore

--- RestFlix/Startup.cs
+++ RestFlix/Startup.cs
@@ -27,7 +27,7 @@ namespace RestFlix
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             //services.AddDbContext<MovieContext>(options =>
                 //options.UseNpgsql(Configuration.GetConnectionString("RestFlixDB")));
             services.AddDbContext<MovieContext>(option => option.UseInMemoryDatabase("MoviesList"));
